46c2278c0fe43e1c642e5e0ebad27b584ad225a7
shiminasai <pablo.ismael90@gmail.com>
2018-09-17 21:29:33 -0600

    agregando paginas

--- js/script.js
+++ js/script.js
@@ -281,6 +281,34 @@
 			}
 		});    		
 	}
+	if ($('.video-relacion').length) {
+		$('.video-relacion').owlCarousel({
+			loop:true,
+			margin:0,
+			dots:false,
+			nav:true,
+			smartSpeed: 500,
+			autoplay: false,
+			navText: [ '<span class="fa fa-angle-left"></span>', '<span class="fa fa-angle-right"></span>' ],
+			responsive:{
+				0:{
+					items:1
+				},
+				480:{
+					items:1
+				},
+				600:{
+					items:1
+				},
+				800:{
+					items:1
+				},
+				1200:{
+					items:1
+				}
+			}
+		});    		
+	}
 	if ($('.video-owl').length) {
 		$('.video-owl').owlCarousel({
 			loop:true,
