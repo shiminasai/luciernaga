540f8fd0fda660c05241d0bee837cfc87e244ebc
crocha <crocha09.09@gmail.com.com>
2018-10-07 16:42:53 -0600

    mas sobre index

--- noticias/views.py
+++ noticias/views.py
@@ -2,6 +2,8 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView, TemplateView
 from .models import Noticias, Temas
+from eventos.models import Eventos
+from videoteca.models import Videotecas
 
 # Create your views here.
 
@@ -11,5 +13,8 @@ class HomeView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['noticias'] = Noticias.objects.all().order_by('fecha')[:4]
+        context['noticias'] = Noticias.objects.all().order_by('-fecha')[:4]
+        context['eventos'] = Eventos.objects.all().order_by('-fecha_inicio')[:8]
+        context['videotecas'] = Videotecas.objects.all().order_by('-id')[:8]
+
         return context
