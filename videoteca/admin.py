11fb31f450a8e1afa30287d2339ae3d99da7075f
Carlos Rocha <crocha09.09@gmail.com>
2018-11-04 19:18:22 -0600

    fix videoteca modelo

--- videoteca/admin.py
+++ videoteca/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from import_export import resources
-from .models import Generos, Idiomas, Temas, Videotecas
+from .models import Generos, Idiomas, Temas, Videotecas, Series, Pais
 from import_export.admin import ImportExportModelAdmin
 
 class VideotecasResource(resources.ModelResource):
@@ -8,11 +8,48 @@ class VideotecasResource(resources.ModelResource):
         model = Videotecas
         exclude = ('slug', )
 
+class GenerosResource(resources.ModelResource):
+    class Meta:
+        model = Generos
+
+class IdiomasResource(resources.ModelResource):
+    class Meta:
+        model = Idiomas
+
+class TemasResource(resources.ModelResource):
+    class Meta:
+        model = Temas
+
+class SeriesResource(resources.ModelResource):
+    class Meta:
+        model = Series
+
+class PaisResource(resources.ModelResource):
+    class Meta:
+        model = Pais
+
+class GenerosAdmin(ImportExportModelAdmin):
+    resource_class = GenerosResource
+
+class IdiomasAdmin(ImportExportModelAdmin):
+    resource_class = IdiomasResource
+
+class TemasAdmin(ImportExportModelAdmin):
+    resource_class = TemasResource
+
+class SeriesAdmin(ImportExportModelAdmin):
+    resource_class = SeriesResource
+
+class PaisAdmin(ImportExportModelAdmin):
+    resource_class = PaisResource
+
 class VideotecasAdmin(ImportExportModelAdmin):
     resource_class = VideotecasResource
 
 # Register your models here.
-admin.site.register(Generos)
-admin.site.register(Idiomas)
-admin.site.register(Temas)
+admin.site.register(Generos, GenerosAdmin)
+admin.site.register(Idiomas, IdiomasAdmin)
+admin.site.register(Temas, TemasAdmin)
+admin.site.register(Series, SeriesAdmin)
+admin.site.register(Pais, PaisAdmin)
 admin.site.register(Videotecas, VideotecasAdmin)
--- videoteca/models.py
+++ videoteca/models.py
@@ -35,31 +35,66 @@ class Temas(models.Model):
         verbose_name = 'Tema'
         verbose_name_plural = 'Temas'
 
+class Coleccion(models.Model):
+    nombre = models.CharField(max_length=250)
+
+    def __str__(self):
+        return self.nombre
+
+    class Meta:
+        verbose_name = 'Colleción'
+        verbose_name_plural = 'Collecciones'
+
+class Pais(models.Model):
+    nombre = models.CharField(max_length=250)
+
+    def __str__(self):
+        return self.nombre
+
+    class Meta:
+        verbose_name = 'Pais'
+        verbose_name_plural = 'Paises'
+
+class Series(models.Model):
+    nombre = models.CharField(max_length=250)
+
+    def __str__(self):
+        return self.nombre
+
+    class Meta:
+        verbose_name = 'serie'
+        verbose_name_plural = 'series'
+
 class Videotecas(models.Model):
     cod_cat = models.CharField('Codigo categoría', max_length=50)
     fecha = models.DateTimeField(auto_now_add=True)
     titulo = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250, editable=False)
-    anio = models.CharField('Año', max_length=50)
-    apoyo = models.CharField('Apoyo de', max_length=50)
-    coleccion = models.CharField('Colección', max_length=250)
+    coleccion = models.ForeignKey(Coleccion, on_delete=models.CASCADE)
     duracion = models.CharField('Duración' , max_length=150)
     edicion = models.CharField('Edición', max_length=250, null=True, blank=True)
     fotografia = models.CharField('Fotografía', max_length=250)
-    genero = models.ForeignKey(Generos, on_delete=models.CASCADE,)
+    genero = models.ForeignKey(Generos, on_delete=models.CASCADE)
     guion = models.CharField('Guión', max_length=250, null=True, blank=True)
-    idioma = models.ForeignKey(Idiomas, on_delete=models.CASCADE,)
+    idioma = models.ForeignKey(Idiomas, on_delete=models.CASCADE)
     musica_original = models.CharField('Música original', max_length=250, null=True, blank=True)
     observaciones = models.TextField(null=True, blank=True)
-    pais_prod = models.CharField('Pais de producción', max_length=250, null=True, blank=True)
-    pais_ref = models.CharField('Pais de referidos', max_length=250, null=True, blank=True)
+    pais_prod = models.ForeignKey( Pais, on_delete=models.CASCADE,
+                            verbose_name='Pais de producción',  related_name='pais_produccion', null=True, blank=True)
+    pais_ref = models.ForeignKey(Pais, on_delete=models.CASCADE,
+                            verbose_name='Pais de referidos', related_name='pais_referidos', null=True, blank=True)
     produccion = models.CharField('Producción', max_length=250, null=True, blank=True)
     productora = models.CharField('Productora', max_length=250)
     realizacion = models.CharField('Realización', max_length=250)
-    serie = models.CharField(max_length=250)
+    serie = models.ForeignKey(Series, on_delete=models.CASCADE, null=True, blank=True)
     sintesis = models.TextField('Síntesis')
     subtitulos = models.CharField('Subtítulos', max_length=250)
     temas = models.ManyToManyField(Temas, verbose_name='Temas finales', blank=True)
+    url_video = models.URLField(null=True, blank=True)
+    imagen_portada = models.ImageField()
+    imagen_fill = models.ImageField()
+    #anio = models.CharField('Año', max_length=50)
+    #apoyo = models.CharField('Apoyo de', max_length=50)
 
     def save(self, *args, **kwargs):
       self.slug = defaultfilters.slugify(self.titulo)
@@ -71,3 +106,4 @@ class Videotecas(models.Model):
     class Meta:
         verbose_name = 'Videoteca'
         verbose_name_plural = 'Videotecas'
+
