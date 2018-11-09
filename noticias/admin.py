272823e45fd84ce66a89143dbd337a3ef54edd80
crocha <crocha09.09@gmail.com.com>
2018-11-09 10:09:28 -0600

    agregando el editor al paginas estaticas

--- noticias/admin.py
+++ noticias/admin.py
@@ -1,6 +1,25 @@
 from django.contrib import admin
+from django.contrib.flatpages.admin import FlatpageForm, FlatPageAdmin
+from django.contrib.flatpages.models import FlatPage
+from django import forms
 from .models import Noticias, Temas
 from sorl.thumbnail.admin import AdminImageMixin
+from tinymce import TinyMCE
+
+class FlatPageForm(forms.ModelForm):
+    content = forms.CharField(widget=TinyMCE(attrs={'cols': 80, 'rows': 30}))
+
+    class Meta:
+        model = FlatPage
+        fields = '__all__'
+
+
+class PageAdmin(FlatPageAdmin):
+    form = FlatPageForm
+
+
+admin.site.unregister(FlatPage)
+admin.site.register(FlatPage, PageAdmin)
 
 class AdminNoticias(AdminImageMixin, admin.ModelAdmin):
     '''
@@ -12,3 +31,5 @@ class AdminNoticias(AdminImageMixin, admin.ModelAdmin):
 
 admin.site.register(Noticias, AdminNoticias)
 admin.site.register(Temas)
+
+
