9bf6c7cbbf5c764c6f5ad3aa2280f2b6efe1c25f
crocha <crocha09.09@gmail.com.com>
2019-03-14 16:51:37 -0600

    agregando busqueda en videoteca

---
+++ videoteca/forms.py
@@ -0,0 +1,4 @@
+from django import forms
+
+class BusquedaVideoteca(forms.Form)):
+    pass
--- videoteca/views.py
+++ videoteca/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .forms import BusquedaVideoteca
 
 # Create your views here.
+
+def busqueda_videoteca(request, template='videoteca.html'):
+
+    return render(request, template, locals())
