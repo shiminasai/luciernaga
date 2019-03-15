00fabae66b43d3e1670f39beb86b32d3171a1140
crocha <crocha09.09@gmail.com.com>
2019-03-15 11:52:47 -0600

    agregando el formulario de busqueda

--- videoteca/forms.py
+++ videoteca/forms.py
@@ -1,4 +1,14 @@
+# -*- coding: utf-8 -*-
+
 from django import forms
+from .models import Generos, Series, Temas, Pais, Idiomas
 
 class BusquedaVideoteca(forms.Form)):
-    pass
+    codigo_cat = forms.CharField(max_length=250)
+    titulo = forms.CharField(max_length=250)
+    serie = forms.ModelChoiceField(queryset=Series.objects.all(), required=False)
+    genero = forms.ModelChoiceField(queryset=Generos.objects.all(), required=False)
+    temas = forms.ModelChoiceField(queryset=Temas.objects.all(), required=False)
+    pais = forms.ModelChoiceField(queryset=Pais.objects.all(), required=False)
+    anio = forms.CharField('Año', max_length=250)
+    idioma = forms.ModelChoiceField(queryset=Idiomas.objects.all(), required=False)
--- videoteca/views.py
+++ videoteca/views.py
@@ -4,5 +4,35 @@ from .forms import BusquedaVideoteca
 # Create your views here.
 
 def busqueda_videoteca(request, template='videoteca.html'):
+    params = {}
+    if request.method == 'POST':
+        params['cod_cat__contains'] = request.POST.get('cod_cat')
+        params['titulo__icontains'] = request.POST.get('titulo')
+        params['serie__nombre__contains'] = request.POST.get('serie')
+        params['genero__nombre__contains'] = request.POST.get('genero')
+        params['temas__nombre__contains'] = request.POST.get('temas')
+        params['pais__nombre__contains'] = request.POST.get('pais')
+        params['anio__icontains'] = request.POST.get('anio')
+        params['idioma__nombre__icontains'] = request.POST.get('idioma')
+
+    unvalid_keys = []
+    for key in params:
+        if not params[key]:
+            unvalid_keys.append(key)
+
+    for key in unvalid_keys:
+        del params[key]
+
+    videoteca_list = Videotecas.objects.filter(**params)
+
+    paginator = Paginator(videoteca_list, 10)
+    page = request.GET.get('page')
+    try:
+        object_list = paginator.page(page)
+    except PageNotAnInteger:
+        object_list = paginator.page(1)
+    except EmptyPage:
+        object_list = paginator.page(paginator.num_pages)
+
 
     return render(request, template, locals())
