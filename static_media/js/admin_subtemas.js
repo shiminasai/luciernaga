19dc68faaf30b2edf474b4e049395bcbe6c8178e
Carlos Rocha <crocha09.09@gmail.com>
2019-04-24 21:26:38 -0600

    fix js multiselect

--- static_media/js/admin_subtemas.js
+++ static_media/js/admin_subtemas.js
@@ -1,28 +1,28 @@
 (function($) {
 
-var carlos = $('#id_subtema').select2('data');
 var currentLocation = window.location.pathname.split('/');
-//alert(currentLocation[5]);
 
 var foo = [];
 $(document).on('change','#id_temas',function(){
 
+    var count = $('#id_subtema option:selected').length;
+    console.log(count);
+
     $('#id_temas :selected').each(function(i, selected){
         foo[i] = $(selected).val();
-        //console.log(foo[i]);
     });
 
     $.getJSON('/ajax/subtemas/?ids='+foo.join(","), function(data){
-        //console.log(data);
-        if (currentLocation[5] == 'change' )
+
+        if (currentLocation[5] == 'change' && count >= 1)
         {
-            $('#id_subtema').html('');
+
         }else {
             $('#id_subtema').html('');
         }
-
+        //$('#id_subtema').html('');
         $('#id_subtema').select2();
-        var subtemas = $('#id_subtema')
+        var subtemas = $('#id_subtema');
         if(data){
 
            $.each(data, function(i, item){
