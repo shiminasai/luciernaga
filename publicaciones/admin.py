f084f602a10c02cb9a4acf6209c9d016288f11e6
shiminasai <pablo.ismael90@gmail.com>
2019-04-30 11:12:14 -0600

    fix url videoteca y publicaciones

--- publicaciones/admin.py
+++ publicaciones/admin.py
@@ -1,13 +1,16 @@
 from django.contrib import admin
-from .models import ArchivosPublicaciones, Publicaciones
+from .models import ArchivosPublicaciones, Publicaciones, EnlaceVideoteca
 # Register your models here.
 
 class ArchivosInlines(admin.TabularInline):
     model = ArchivosPublicaciones
     extra = 1
+class EnlaceInlines(admin.TabularInline):
+    model = EnlaceVideoteca
+    extra = 1
 
 class PublicacionesAdmin(admin.ModelAdmin):
-    inlines = [ArchivosInlines]
+    inlines = [ArchivosInlines, EnlaceInlines]
     list_display = ['titulo', 'year', 'autores', '_tiene_pdf']
     search_fields = ('titulo', 'autores',)
 
